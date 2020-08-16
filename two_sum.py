863c46a87564232e43ad8f0f5c263a816e2aec95
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-08-16 15:34:32 +0100

    add solution of O(log n) if array is sorted :+1:

--- two_sum.py
+++ two_sum.py
@@ -6,7 +6,7 @@
 # return [0, 1].
 
 
-# Brute force approach (On2)
+# Brute force approach O(n*2)
 class Solution:
     def twoSum(self, nums, target: int):
         for i in range(len(nums)):
@@ -18,7 +18,7 @@ class Solution:
           return []
 
 
-# Better approach (On)
+# Better approach O(n)
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -41,3 +41,24 @@ class Solution:
 
 my_solution = Solution()
 print(my_solution.twoSum([2,11,15, 7, -2], 9))
+
+
+# Best if array is already sorted O(log n)
+# if array is not sorted O(n log n)
+class Solution:
+    @classmethod
+    def find_two_sum(cls, arr, target):
+        last = len(arr) - 1
+        first = 0
+        arr = sorted(arr) # O(n)
+
+        while (first < last): # O(log n)
+            if ((arr[first] + arr[last]) > target):
+                last -= 1
+            elif((arr[first] + arr[last]) < target):
+                first += 1
+            elif((arr[first] + arr[last]) == target):
+                return True
+        return False
+
+print(Solution.find_two_sum([1,2,3,4], 8)) # O(n log n)
